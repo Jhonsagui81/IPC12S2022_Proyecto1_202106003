92b67d38e2107fc03034ded011cd1926e84be6f2
Jhonatan <reyesjhonatan86@gmail.com>
2022-09-01 21:51:00 -0600

    Proyecto1 - Terminado

---
+++ Proyecto1/src/proyecto1/Cuentas.java
@@ -0,0 +1,64 @@
+package proyecto1;
+
+import java.time.LocalDateTime;
+
+public class Cuentas {
+    // atributos
+    private int idCuenta;
+    private double SaldoCuenta;
+    Historial  operacion [] = new Historial[20]; 
+    int ContadorTransacciones;
+    
+    
+    //constructores
+    Cuentas() {
+        this.idCuenta = 0;
+        this.SaldoCuenta = 0;
+    }
+
+    public Cuentas(int idCuenta) {
+        this.idCuenta = (idCuenta+1);
+        this.ContadorTransacciones = 0;
+    }
+    //METODOS
+    
+    public void AsociarOperacion(int idHistorial, LocalDateTime fechaHoraActuales, String Descripcion, double MontoDebitado, double MontoAcreditado, double saldoActual){
+        if(ContadorTransacciones < operacion.length){
+            operacion[ContadorTransacciones] = new Historial(idHistorial, fechaHoraActuales, Descripcion, MontoDebitado, MontoAcreditado, saldoActual);
+            ContadorTransacciones++;
+        }
+    }
+    
+    
+//    public boolean transferir(double valor, Cuentas idCuentaDestino) {
+//        if (this.SaldoCuenta >= valor) {
+//            this.SaldoCuenta -= valor;
+//            idCuentaDestino.setSaldoCuenta(valor);
+//            return true;
+//        } else {
+//            return false;
+//        }
+//
+//    }
+    
+    public void retirarSaldo(double SaldoCuenta){
+        this.SaldoCuenta -= SaldoCuenta;
+    }
+    
+    //get y set
+    public int getIdCuenta() {
+        return idCuenta;
+    }
+
+    public void setIdCuenta(int idCuenta) {
+        this.idCuenta = idCuenta;
+    }
+
+    public double getSaldoCuenta() {
+        return SaldoCuenta;
+    }
+    
+    public void setSaldoCuenta(double SaldoCuenta) {
+        this.SaldoCuenta += SaldoCuenta;
+    }
+}
